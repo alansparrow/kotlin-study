3f8cb01adbe2cce493d538641a5719da4d071f00
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-20 23:58:17 +0100

    Elvis operator: ?:

--- Null/src/main/kotlin/Main.kt
+++ Null/src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
     var favoriteActor: String? = "Sandra Oh"
-    println(favoriteActor!!.length)
 
-    var favoriteActor1: String? = null
-    println(favoriteActor1!!.length)
+    var lengthOfName = favoriteActor?.length ?: 0
+    println("Length is: $lengthOfName")
+
+    favoriteActor = null
+    lengthOfName = favoriteActor?.length ?: 0
+    println("Length is: $lengthOfName")
 }
