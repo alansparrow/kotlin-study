df09e7081eebd244305122e852f7837b40482174
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-21 21:56:18 +0100

    define class methods

--- SmartHome/src/main/kotlin/Main.kt
+++ SmartHome/src/main/kotlin/Main.kt
@@ -1,7 +1,15 @@
 class SmartDevice {
+    fun turnOn() {
+        println("Smart device is turned on.")
+    }
 
+    fun turnOff() {
+        println("Smart device is turned off.")
+    }
 }
 
 fun main(args: Array<String>) {
     val smartTvDevice = SmartDevice()
+    smartTvDevice.turnOn()
+    smartTvDevice.turnOff()
 }
