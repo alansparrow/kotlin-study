af6d51a84082e6443e1bc03eaf0cbbc06b424386
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-21 22:11:16 +0100

    define a constructor

--- SmartHome/src/main/kotlin/Main.kt
+++ SmartHome/src/main/kotlin/Main.kt
@@ -1,4 +1,14 @@
-class SmartDevice {
+class SmartDevice constructor(val name: String, val category: String) {
+    var deviceStatus = "online"
+
+    constructor(name: String, category: String, statusCode: Int) : this(name, category) {
+        deviceStatus = when (statusCode) {
+            0 -> "offline"
+            1 -> "online"
+            else -> "unknown"
+        }
+    }
+
     fun turnOn() {
         println("Smart device is turned on.")
     }
@@ -9,7 +19,9 @@ class SmartDevice {
 }
 
 fun main(args: Array<String>) {
-    val smartTvDevice = SmartDevice()
+    val smartTvDevice = SmartDevice("Android TV","Entertainment")
+    println("Device name is: ${smartTvDevice.name}")
+    println("Device category is: ${smartTvDevice.category}")
     smartTvDevice.turnOn()
     smartTvDevice.turnOff()
 }
