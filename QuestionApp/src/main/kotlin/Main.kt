1f94e2d664e4e97c1ca45bf6241bd64403abe372
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-27 00:43:20 +0100

    use companion object

--- QuestionApp/src/main/kotlin/Main.kt
+++ QuestionApp/src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
-object StudentProgress {
-    var total: Int = 10
-    var answered: Int = 3
-}
-
 enum class Difficulty {
     EASY, MEDIUM, HARD
 }
@@ -13,13 +8,21 @@ data class Question<T>(
     val difficulty: Difficulty
 )
 
-fun main(args: Array<String>) {
+class Quiz {
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 
-    println(question1)
-    println(question2)
-    println(question3)
-    println("${StudentProgress.answered} of ${StudentProgress.total} answered.")
+    companion object StudentProgress {
+        var total: Int = 10
+        var answered: Int = 3
+    }
+}
+
+fun main(args: Array<String>) {
+    val quiz = Quiz()
+    println(quiz.question1)
+    println(quiz.question2)
+    println(quiz.question3)
+    println("${Quiz.answered} of ${Quiz.total} answered.")
 }
