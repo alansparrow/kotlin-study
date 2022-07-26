07b6a2f1aaa4b95ee926a5daaf70179903ee9398
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-26 23:36:55 +0100

    use enum class for difficulty

--- QuestionApp/src/main/kotlin/Main.kt
+++ QuestionApp/src/main/kotlin/Main.kt
@@ -1,13 +1,17 @@
+enum class Difficulty {
+    EASY, MEDIUM, HARD
+}
+
 class Question<T>(
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 
 fun main(args: Array<String>) {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 
     println(question1)
     println(question2)
