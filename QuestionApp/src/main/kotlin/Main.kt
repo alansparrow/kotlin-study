40cabf51185098a4103010c4d9ca4d3b2abf6ddc
Fuzzy Rock <trannguyenbaotrung@gmail.com>
2022-07-29 01:08:56 +0100

    add an extension function

--- QuestionApp/src/main/kotlin/Main.kt
+++ QuestionApp/src/main/kotlin/Main.kt
@@ -22,10 +22,17 @@ class Quiz {
 val Quiz.StudentProgress.progressText: String
     get() = "$answered of $total answered"
 
+fun Quiz.StudentProgress.printProgressBar() {
+    repeat(Quiz.answered) { print("▓") }
+    repeat(Quiz.total - Quiz.answered) { print("▒") }
+    println()
+    println(Quiz.progressText)
+}
+
 fun main(args: Array<String>) {
     val quiz = Quiz()
     println(quiz.question1)
     println(quiz.question2)
     println(quiz.question3)
-    println(Quiz.progressText)
+    Quiz.printProgressBar()
 }
